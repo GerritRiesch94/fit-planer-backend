6cbb2a4b55f86b0d8c5a38cf758d9983a2e61637
GerritRiesch94 <gerrit.riesch@live.de>
2023-01-05 12:56:16 +0100

    feat(athlete): add simple athlete controller and enable cors standard settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AthleteController } from './athlete/athlete.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, AthleteController],
   providers: [AppService],
 })
 export class AppModule {}
