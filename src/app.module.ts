8fe51efd11510b70112a8138f90b93a5516963fb
GerritRiesch94 <gerrit.riesch@live.de>
2023-01-06 23:42:17 +0100

    feat(athlete db model): add first draft of database service for athlete model

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AthleteController } from './athlete/athlete.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Athlete, AthleteSchema } from './athlete/database/athlete.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    MongooseModule.forFeature([{ name: Athlete.name, schema: AthleteSchema }]),
+  ],
   controllers: [AppController, AthleteController],
   providers: [AppService],
 })
---
+++ src/athlete/model/request/AthleteRequest.ts
@@ -0,0 +1,7 @@
+export interface AthleteRequest {
+  combinedName: string;
+  age: string;
+  combinedAddress: string;
+  email: string;
+  phoneNumber: string;
+}
