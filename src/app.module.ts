f8f657f14fa8dedd3744cb3422937d6c17528d92
GerritRiesch94 <gerrit.riesch@live.de>
2023-01-13 06:57:45 +0100

    feat(athlete-api): connect find all api with db service and use env files
    call db service to return stored athletes via get api. use env files for
    local development and prod stage to provide different values for mongodb
    uri.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,26 @@ import { AppService } from './app.service';
 import { AthleteController } from './athlete/athlete.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Athlete, AthleteSchema } from './athlete/database/athlete.schema';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AthleteDbServiceService } from './athlete/database/athlete-db-service.service';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    ConfigModule.forRoot({ envFilePath: `env/${process.env.NODE_ENV}.env` }),
+    MongooseModule.forRootAsync({
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: `env/${process.env.NODE_ENV}.env`,
+        }),
+      ],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URI'),
+      }),
+    }),
     MongooseModule.forFeature([{ name: Athlete.name, schema: AthleteSchema }]),
   ],
   controllers: [AppController, AthleteController],
-  providers: [AppService],
+  providers: [AppService, AthleteDbServiceService, ConfigService],
 })
 export class AppModule {}
--- src/athlete/athlete.controller.ts
+++ src/athlete/athlete.controller.ts
@@ -1,26 +1,14 @@
 import { Controller, Get, HttpCode } from '@nestjs/common';
 import { AthleteResponse } from './model/response/AthleteResponse';
+import { AthleteDbServiceService } from './database/athlete-db-service.service';
 
 @Controller('athlete')
 export class AthleteController {
+  constructor(private readonly athleteDbService: AthleteDbServiceService) {}
+
   @Get()
   @HttpCode(200)
-  findAllAthletes(): AthleteResponse[] {
-    return [
-      {
-        combinedName: 'Stefan Sportler',
-        age: '32',
-        combinedAddress: 'Am Acker 31, 90469 Nürnberg',
-        email: 'stefan.sportler@live.de',
-        phoneNumber: '017612345678',
-      },
-      {
-        combinedName: 'Anna Athletin',
-        age: '40',
-        combinedAddress: 'Bergstraße 12, 90411Nürnberg',
-        email: 'anna.athletin@live.de',
-        phoneNumber: '016987654321',
-      },
-    ];
+  async findAllAthletes(): Promise<AthleteResponse[]> {
+    return await this.athleteDbService.findAll();
   }
 }
