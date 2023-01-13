a17c7d3c09ae7c7595cc63203c81a2ecba5af354
GerritRiesch94 <gerrit.riesch@live.de>
2023-01-13 07:44:57 +0100

    feat(athlete-api): add post api to create an athlete

--- src/athlete/athlete.controller.spec.ts
+++ src/athlete/athlete.controller.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AthleteController } from './athlete.controller';
+import { AthleteDbServiceService } from './database/athlete-db-service.service';
 
 describe('AthleteController', () => {
   let controller: AthleteController;
+  const athleteDbServiceMock = { findAll: jest.fn(), create: jest.fn() };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AthleteController],
+      providers: [
+        { provide: AthleteDbServiceService, useValue: athleteDbServiceMock },
+      ],
     }).compile();
 
     controller = module.get<AthleteController>(AthleteController);
@@ -14,5 +19,59 @@ describe('AthleteController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return all athletes for get api', async () => {
+    // arrange
+    const athleteResponse = {
+      combinedName: 'Stefan Sportler',
+      age: '32',
+      combinedAddress: 'Am Acker 31, 90469 Nürnberg',
+      email: 'stefan.sportler@live.de',
+      phoneNumber: '017612345678',
+    };
+
+    const findAllSpy = jest
+      .spyOn(athleteDbServiceMock, 'findAll')
+      .mockReturnValue(athleteResponse);
+
+    // act
+    const result = await controller.findAllAthletes();
+
+    // assert
+
+    expect(result).toStrictEqual(athleteResponse);
+    expect(findAllSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should successfully create athlete for post api', async () => {
+    // arrange
+    const athleteResponse = {
+      combinedName: 'Stefan Sportler',
+      age: '32',
+      combinedAddress: 'Am Acker 31, 90469 Nürnberg',
+      email: 'stefan.sportler@live.de',
+      phoneNumber: '017612345678',
+    };
+    const createSpy = jest
+      .spyOn(athleteDbServiceMock, 'create')
+      .mockReturnValue(athleteResponse);
+
+    const athleteToCreate = {
+      combinedName: 'Stefan Sportler',
+      age: '32',
+      combinedAddress: 'Am Acker 31, 90469 Nürnberg',
+      email: 'stefan.sportler@live.de',
+      phoneNumber: '017612345678',
+    };
+
+    // act
+
+    const result = await controller.createAthlete(athleteToCreate);
+
+    // assert
+    expect(result).toStrictEqual(athleteResponse);
+    expect(createSpy).toHaveBeenCalledTimes(1);
+    expect(createSpy).toBeCalledWith(athleteToCreate);
   });
 });
--- src/athlete/athlete.controller.ts
+++ src/athlete/athlete.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, HttpCode } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
 import { AthleteResponse } from './model/response/AthleteResponse';
 import { AthleteDbServiceService } from './database/athlete-db-service.service';
+import { AthleteRequest } from './model/request/AthleteRequest';
 
 @Controller('athlete')
 export class AthleteController {
@@ -10,5 +11,13 @@ export class AthleteController {
   @HttpCode(200)
   async findAllAthletes(): Promise<AthleteResponse[]> {
     return await this.athleteDbService.findAll();
+  }
+
+  @Post()
+  @HttpCode(201)
+  async createAthlete(
+    @Body() athleteRequest: AthleteRequest,
+  ): Promise<AthleteResponse> {
+    return await this.athleteDbService.create(athleteRequest);
   }
 }
