6f2d4aacdea6c040a47583ff64cc818c9b6e44af
Gerrit Riesch <gerrit.riesch@live.de>
2023-04-04 17:53:33 +0200

    docs(swagger doc): add swagger doc annotations for app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
 import { Controller, Get, Param, Query, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('app-controller')
 @Controller('api/data')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get(':id')
+  @ApiOperation({ description: 'Get a list of strings' })
+  @ApiResponse({
+    status: 200,
+    description: 'return list of strings for id and pagination params',
+    type: [String],
+  })
+  @ApiResponse({
+    status: 204,
+    description: 'empty list found for given parameter',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'no resources found for given id',
+  })
   async getData(
     @Param('id') id: string,
     @Query('skip') skip: number,
